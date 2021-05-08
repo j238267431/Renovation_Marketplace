6f16f89e360f180550d56358c048915a342f9ff4
andykey-krsk <andykey@bk.ru>
2021-05-08 14:57:52 +0700

    fix css and js

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
   <!-- fonts -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
         integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
@@ -12,7 +13,9 @@
   <!-- styles -->
   <link rel="stylesheet" href="/css/app.css">
 
+  <!-- scripts -->
   <script src="/js/app.js"></script>
+
   <title>СтройХаус</title>
 </head>
 
@@ -26,16 +29,6 @@
 
     <x-main-footer></x-main-footer>
   </div>
-
-  <!-- scripts -->
-  <script src="/js/jquery.js"></script>
-  <script src="/js/jquery.visible.min.js"></script>
-  <script src="/js/popper.min.js"></script>
-  <script src="/js/tether.min.js"></script>
-  <script src="/js/bootstrap.js"></script>
-  <script src="/js/bootstrap-typeahead.min.js"></script>
-  <script src="/js/offer.js"></script>
-
 
   <!-- summernote css/js -->
   <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
